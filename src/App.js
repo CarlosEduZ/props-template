32fa98eac3004e51dfbd256e254f895d01bb3d81
Carlos Eduardo <eduardo-1942@hotmail.com>
2022-10-17 21:00:25 -0300

    aula de props

--- src/App.js
+++ src/App.js
@@ -2,9 +2,66 @@ import Garagem from "./Componentes/Garagem";
 import "./styles.css";
 
 export default function App() {
+  const carro1 = {
+    nomeDoCarro: "Fusca",
+    ano: 1970,
+    cor: "Azul",
+    flex: false
+  }
+  const carro2 = {
+    nomeDoCarro: "Brasília",
+    ano: 1965,
+    cor: "Amarela",
+    flex: false
+  }
+  const carro3 = {
+    nomeDoCarro: "Opala",
+    ano: 1984,
+    cor: "Verde",
+    flex: true
+  }
+  const carro4 = {
+    nomeDoCarro: "Siena",
+    ano: 2012,
+    cor: "Prata",
+    flex: true
+  }
+  const carro5 = {
+    nomeDoCarro: "Gol",
+    ano: 2001,
+    cor: "Cinza",
+    flex: false
+  }
+  const carro6 = {
+    nomeDoCarro: "Voyage",
+    ano: 2010,
+    cor: "Preto",
+    flex: true
+  }
+  const carro7 = {
+    nomeDoCarro: "Ferrari",
+    ano: 2022,
+    cor: "Vermelho",
+    flex: true
+  }
+
+  function apresentacao(nome){
+    alert(`Bem-vindos á garagem de ${nome}`)
+  }
+  
   return (
     <div>
-      <Garagem />
+      <Garagem 
+      nome={"Carlos"}
+      fusca={carro1}
+      brasilia={carro2}
+      opala={carro3}
+      funcao={apresentacao}
+      siena={carro4}
+      gol={carro5}
+      voyage={carro6}
+      ferrari={carro7}
+      />
     </div>
   );
 }
--- src/Componentes/Garagem.js
+++ src/Componentes/Garagem.js
@@ -1,14 +1,39 @@
 import Carro from "./Carro";
 
-function Garagem() {
+function Garagem(props) {
   return (
     <div>
-      <h1>Garagem de Indio</h1>
-      <Carro />
-      <Carro />
-      <Carro />
-      <Carro />
+      <h1>Garagem de {props.nome} </h1>
+      <button onClick={()=>props.funcao(props.nome)}>Boas Vindas</button>
+      <Carro 
+      carro={props.fusca}
+      />
+      <Carro 
+      carro={props.brasilia}
+      />
+      <Carro 
+      carro={props.opala}
+      />
+
+
+      <h1> Segunda Garagem do {props.nome}</h1>
+      <button onClick={()=>props.funcao(props.nome)} >Boas Vindas</button>
+      <Carro
+      carro={props.siena}
+      />
+      <Carro
+      carro={props.gol}
+      />
+      <Carro
+      carro={props.voyage}
+      />
+      <Carro
+      carro={props.ferrari}
+      />
+    
+      
     </div>
+
   );
 }
 
